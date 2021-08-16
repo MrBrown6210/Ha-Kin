b09d4811ead5557565c9074dd360787024c35ca3
MrBrown6210 <bite6210@gmail.com>
2021-08-16 23:29:46 +0700

    Add style tp RestaurantCard component

--- components/RestaurantCard.tsx
+++ components/RestaurantCard.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent } from "react";
 
+import X from "../public/star-empty.svg";
+
 export type RestaurantProps = {
   restaurant: {
     title: string;
@@ -17,11 +19,28 @@ const RestaurantCard: FunctionComponent<RestaurantProps> = ({ restaurant }) => {
         <img className="bg-gray-200" alt="image" />
       </div>
       <div>
-        <div>{restaurant.title}</div>
-        <div>Star</div>
-        <div>{restaurant.reviewers} reviewed</div>
-        <div>{restaurant.tags.join()}</div>
-        <div>{Array(restaurant.averageCost).fill("$").join("")}</div>
+        <div className="mt-2 text-lg leading-6 font-semibold">
+          {restaurant.title}
+        </div>
+        <div className="flex mt-1">
+          {Array(restaurant.stars).fill(
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7 text-yellow-300"
+              viewBox="2 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>
+          )}
+        </div>
+        <div className="text-sm leading-none text-gray-600">
+          {restaurant.reviewers} reviewed
+        </div>
+        <div className="mt-1 text-gray-700">{restaurant.tags.join()}</div>
+        <div className="flex space-x-1">
+          {Array(restaurant.averageCost).fill(<div>$</div>)}
+        </div>
       </div>
     </div>
   );
