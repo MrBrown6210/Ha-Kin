4c19a0f1ed80ee257edfc6dc4cab3757bdff36f0
MrBrown6210 <bite6210@gmail.com>
2021-08-17 20:55:46 +0700

    Change fistures to function

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,6 +1,6 @@
 import { rest } from "msw";
 import { clients } from "./fixtures/feature-clients";
-import { restaurants } from "./fixtures/restaurants";
+import { getRestaurants } from "./fixtures/restaurants";
 
 const MOCK_API = "https://msw.io";
 
@@ -9,6 +9,16 @@ export const handlers = [
     return res(ctx.delay(), ctx.json(clients));
   }),
   rest.get(`${MOCK_API}/restaurants`, (req, res, ctx) => {
+    const restaurants = getRestaurants();
     return res(ctx.delay(), ctx.json(restaurants));
+  }),
+  rest.get(`${MOCK_API}/restaurants/:slug`, (req, res, ctx) => {
+    const slug = req.params.slug;
+    const restaurants = getRestaurants();
+    const restaurant = restaurants.find(
+      (restaurant) => restaurant.slug === slug
+    );
+    if (!restaurant) return res(ctx.delay(), ctx.status(404));
+    return res(ctx.delay(), ctx.json(restaurant));
   }),
 ];
