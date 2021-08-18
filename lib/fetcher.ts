d80f0e115316a7180149a3c2bd68d9b4d94e75be
MrBrown6210 <bite6210@gmail.com>
2021-08-18 11:57:38 +0700

    Add cookie for fetch auth when reload

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,4 +1,6 @@
 import Axios, { AxiosRequestConfig } from "axios";
+import Cookie from "js-cookie";
+
 const API_URL = process.env.API_URL || "https://msw.io";
 
 export const axios = Axios.create({
@@ -8,6 +10,9 @@ export const axios = Axios.create({
     "Content-Type": "application/json",
   },
 });
+
+const token = Cookie.get("token");
+if (token) axios.defaults.headers.Authorization = `Bearer ${token}`;
 
 export const fetcher = async <T>(url: string, arg?: AxiosRequestConfig) => {
   const res = await axios.get(url, arg);
