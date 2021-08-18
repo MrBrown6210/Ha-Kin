6a3390208d1514e554c6d396d3c5515e29891a38
MrBrown6210 <bite6210@gmail.com>
2021-08-18 11:03:58 +0700

    Remove unused code

--- components/FeatureClientList.tsx
+++ components/FeatureClientList.tsx
@@ -1,14 +1,8 @@
 import { FunctionComponent } from "react";
 import useSWR from "swr";
 import { IClient } from "../lib/api.interface";
-import { fetcher } from "../lib/fetcher";
 
 type Props = {};
-
-// export async function getServerSideProps(): Promise<{ props: Props }> {
-//   const clients: IClient[] = await fetcher("restaurants");
-//   return { props: { clients } };
-// }
 
 const CustomComponent: FunctionComponent<Props> = (props) => {
   const { data: clients, error } = useSWR<IClient[]>("feature-clients");
