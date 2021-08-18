4bb7dffc543ee123d29552ef8ff6efe8f22986dc
MrBrown6210 <bite6210@gmail.com>
2021-08-18 11:08:04 +0700

    Change fetcher from fetch to axios

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,7 +1,18 @@
+import Axios, { AxiosRequestConfig } from "axios";
 const API_URL = process.env.API_URL || "https://msw.io";
 
-export const fetcher = async <T>(url: string, arg?: RequestInit) => {
-  const api = url.startsWith("http") ? url : `${API_URL}/${url}`;
-  const res = await fetch(api, arg);
-  return await res.json();
+export const axios = Axios.create({
+  baseURL: API_URL,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export const fetcher = async <T>(url: string, arg?: AxiosRequestConfig) => {
+  const res = await axios.get(url, arg);
+  return res.data;
+  // const api = url.startsWith("http") ? url : `${API_URL}/${url}`;
+  // const res = await fetch(api, arg);
+  // return await res.json();
 };
